115a9c75551a2741768b5c980b3f7c23427b563a
Rabi22 <75093181+Rabi22@users.noreply.github.com>
2025-07-07 02:12:58 +0530

    delete script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-document.getElementById(submit).click=()=>{
-    if((document.querySelector(todo_input).innerHtml())!==''){
-        items.push(document.querySelector(todo_input).innerHTML);
-    }
-}
+
